f4a59a08bb13aba7e5d3fd0c2c035285f89920ff
Cesar Reyes <creyes@xik.gt>
2014-10-08 10:48:11 -0600

    Added a layout

--- WQmlToolbar.cpp
+++ WQmlToolbar.cpp
@@ -7,17 +7,46 @@ WQmlToolbar::WQmlToolbar(QWidget *parent) :
 	QDockWidget(parent)
 	{
 	// self flags
-	this->setFeatures(QDockWidget::NoDockWidgetFeatures);
-	setObjectName("Dashboard");
+	setFeatures(QDockWidget::NoDockWidgetFeatures);
+	setObjectName("Toolbar");
 
-	// child qml view
+	QWidget* pwWidgetLayout = new QWidget();	// Since the QDockWidget can handle only one widget, we create a widget with a layout inside
+
+	OLayoutHorizontal *pLayout = new OLayoutHorizontal(pwWidgetLayout);
+	pLayout->setMargin(0);
+
+	pwWidgetLayout->setStyleSheet("background-color: yellow");
+	pwWidgetLayout->setLayout(pLayout);
+
 	QQuickView *pQmlView = new QQuickView();
-	QWidget *pContainerWidget = QWidget::createWindowContainer(pQmlView, this);
-	pContainerWidget->setMinimumSize(1024, 74);
-
-
 	//QQmlEngine *pQmlEngine = pQmlView->engine();
 	pQmlView->setSource(QUrl(QStringLiteral("qrc:/ui/main.qml")));
+
+	QWidget *pContainerWidget = QWidget::createWindowContainer(pQmlView);
+	pLayout->addWidget(pContainerWidget);
+	setWidget(pwWidgetLayout);
+
+
+	#if 0
+	OLayoutVertical * pLayoutVertical = new OLayoutVertical(pwWidgetLayout);		// Vertical layout to stack the profile switcher, search, tree view and the status.
+
+	pwWidgetLayout->setStyleSheet("background-color: yellow");
+	setWidget(pwWidgetLayout);
+
+
+	/*QWidget *pDummy = new QWidget();
+	pLayout->addWidget(pDummy);
+	pDummy->setStyleSheet("background-color: yellow");
+	this->setWidget(pDummy);
+	*/
+
+	// child qml view
+
+
+	pContainerWidget->setMinimumSize(1024, 74);
+	pLayoutVertical->addWidget(pContainerWidget);
+
+	#endif
 
 
 	}
