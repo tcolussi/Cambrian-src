fbecfc2d900066b2eb70ac18069a21e6b1f7735c
CambrianDev <daniel@cambrian.org>
2014-10-17 11:38:09 -0600

    Re-enable QML toolbar

--- WMainWindow.cpp
+++ WMainWindow.cpp
@@ -317,7 +317,7 @@ WMainWindow::WMainWindow() : QMainWindow(),ui(new Ui::startupScreen)
 	#if 1
 	addDockWidget(Qt::RightDockWidgetArea, PA_CHILD new WDashboard);
 	#endif
-	#if 0
+	#if 1
 	addDockWidget(Qt::TopDockWidgetArea, PA_CHILD new WQmlToolbar);
 	#endif
 
